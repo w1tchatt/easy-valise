dcd1c8aca53cbde6c9cd46ee89b38d3e0ccad518
w1tchatt <108813411+w1tchatt@users.noreply.github.com>
2023-04-23 17:23:37 +0200

    Init Xcode project

---
+++ EasyValise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyValise
+//
+//  Created by Richard Sourianarayanane on 23/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
