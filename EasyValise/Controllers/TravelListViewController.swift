a0cbdbb49cbd8969d58881fb555008209445c0da
w1tchatt <108813411+w1tchatt@users.noreply.github.com>
2023-06-08 22:42:43 +0200

    MVVM refactoring TravelList

--- EasyValise/Controllers/TravelListViewController.swift
+++ EasyValise/Controllers/TravelListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TravelListViewController: UIViewController {
     var travel: Travel = Travel(name: "", date: .now, suitcase: Suitcase(items: [Item(name: "", isChecked: false, section: Section.drugs.rawValue)]))
-    private var sectionsName:[String] = []
+    //private var sectionsName:[String] = []
     
     let travelListViewModel = TravelListViewModel()
     
@@ -20,41 +20,44 @@ class TravelListViewController: UIViewController {
         self.listItemsTableView.delegate = self
         self.listItemsTableView.dataSource = self
         
-        defineSectionsToShow()
+        #warning("Logique de faire de renvoyer 'travel' au viewModel ?")
+        travelListViewModel.travel = travel
+        travelListViewModel.defineSectionsToShow()
     }
 }
 
 
 extension TravelListViewController: UITableViewDelegate, UITableViewDataSource {
     
-    private func defineSectionsToShow() {
-        var sectionsUsed: Set<String> = []
-        for item in travel.suitcase.items {
-            sectionsUsed.insert(item.section)
-        }
-        sectionsName = Array(sectionsUsed)
-        sectionsName = sectionsName.sorted { $0 < $1 }
-    }
+//    private func defineSectionsToShow() {
+//        var sectionsUsed: Set<String> = []
+//        for item in travel.suitcase.items {
+//            sectionsUsed.insert(item.section)
+//        }
+//        sectionsName = Array(sectionsUsed)
+//        sectionsName = sectionsName.sorted { $0 < $1 }
+//    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-       return sectionsName.count
+        return travelListViewModel.sectionsName.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionsName[section]
+        return travelListViewModel.sectionsName[section]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let itemsInSection = travel.suitcase.items.filter { $0.section == sectionsName[section] }
+        let itemsInSection = travelListViewModel.travel.suitcase.items.filter { $0.section == travelListViewModel.sectionsName[section] }
         return itemsInSection.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = listItemsTableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-        let section = sectionsName[indexPath.section]
-        let itemsInSection = travel.suitcase.items.filter { $0.section == section }
-        let itemsInSectionOrdered = itemsInSection.sorted { $0.name < $1.name }
-        let item = itemsInSectionOrdered[indexPath.row]
+//        let section = travelListViewModel.sectionsName[indexPath.section]
+//        let itemsInSection = travelListViewModel.travel.suitcase.items.filter { $0.section == section }
+//        let itemsInSectionOrdered = itemsInSection.sorted { $0.name < $1.name }
+//        let item = itemsInSectionOrdered[indexPath.row]
+        let item = travelListViewModel.getItem(indexpathSection: indexPath.section, indexpathRow: indexPath.row)
         var content = cell.defaultContentConfiguration()
         content.text = item.name
         
@@ -81,9 +84,11 @@ extension TravelListViewController: UITableViewDelegate, UITableViewDataSource {
             return
         }
         
-        let itemsInSection = travel.suitcase.items.filter { $0.section == sectionsName[indexPath.section] }
-        let itemsInSectionOrdered = itemsInSection.sorted { $0.name < $1.name }
-        let item = itemsInSectionOrdered[indexPath.row]
+//        let itemsInSection = travelListViewModel.travel.suitcase.items.filter { $0.section == travelListViewModel.sectionsName[indexPath.section] }
+//        let itemsInSectionOrdered = itemsInSection.sorted { $0.name < $1.name }
+//        let item = itemsInSectionOrdered[indexPath.row]
+        
+        let item = travelListViewModel.getItem(indexpathSection: indexPath.section, indexpathRow: indexPath.row)
         
         travelListViewModel.updateIdChecked(for: travel, item: item, isChecked: sender.isOn) { success in
             if success {
--- EasyValise/ViewModels/TravelListViewModel.swift
+++ EasyValise/ViewModels/TravelListViewModel.swift
@@ -10,6 +10,8 @@ import Foundation
 final class TravelListViewModel {
  
     let easyValiseRepository = EasyValiseRepository()
+    var travel: Travel = Travel(name: "", date: .now, suitcase: Suitcase(items: [Item(name: "", isChecked: false, section: Section.drugs.rawValue)]))
+    var sectionsName:[String] = []
     
     func updateIdChecked(for travel: Travel, item: Item, isChecked: Bool, callback: @escaping (Bool) -> Void) {
         easyValiseRepository.updateIsChecked(for: travel, item: item, isChecked: isChecked) { [] success in
@@ -19,6 +21,22 @@ final class TravelListViewModel {
                 callback(false)
             }
         }
+    }
+    
+    func defineSectionsToShow() {
+        var sectionsUsed: Set<String> = []
+        for item in travel.suitcase.items {
+            sectionsUsed.insert(item.section)
+        }
+        self.sectionsName = Array(sectionsUsed)
+        self.sectionsName = sectionsName.sorted { $0 < $1 }
+    }
+    
+    func getItem(indexpathSection:Int, indexpathRow:Int) -> Item {
+        let section = self.sectionsName[indexpathSection]
+        let itemsInSection = self.travel.suitcase.items.filter { $0.section == section }
+        let itemsInSectionOrdered = itemsInSection.sorted { $0.name < $1.name }
+        return itemsInSectionOrdered[indexpathRow]
     }
     
 }
